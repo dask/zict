d8cc7b3abc82e8abc81fdfa53546a0d7da4c8718
Antoine Pitrou <pitrou@free.fr>
2016-11-01 14:23:05 +0100

    Add a Sieve mapping (#5)
    * Add LMDB backend
    
    * Add more tests
    
    * Add optimized version of update()
    
    * Fix del[...] to raise KeyError when the key doesn't exist
    
    * Install python-lmdb in travis-ci
    
    * Fix syntax error on py2
    
    * Initial Sieve implementation
    
    * Improve tests
    
    Factor out tests for the mapping interface and reuse them in most test suites
    (except Zip(), since `del` isn't implemented there)
    
    * Rename z.tests.common to z.tests.utils_test, so that py.test enables its assertion magic
    
    * Fix Sieve.__delitem__ and add tests
    
    * Change API as suggested
    
    * Add optimized update() implementation
    
    * Add stress test for updates and fix a bug in Buffer
    
    * Fix doctest
    
    * Fix docstring
    
    * Strengthen tests a bit more

---
+++ zict/tests/test_sieve.py
@@ -0,0 +1,71 @@
+from __future__ import absolute_import, division, print_function
+
+import sys
+
+from zict import Sieve
+from . import utils_test
+
+
+def test_simple():
+    a = {}
+    b = {}
+    c = {}
+    def selector(k, v):
+        return len(v) % 3
+    mappings = {0: a, 1: b, 2: c}
+
+    d = Sieve(mappings, selector)
+    assert len(d) == 0
+
+    d['u'] = b'the'
+    d['v'] = b'big'
+    d['w'] = b'brown'
+    d['x'] = b'fox'
+    d['y'] = b'jumps'
+    d['z'] = b'over'
+
+    assert d['u'] == b'the'
+    assert d['v'] == b'big'
+    assert len(d) == 6
+
+    assert sorted(d) == ['u', 'v', 'w', 'x', 'y', 'z']
+    assert sorted(d.keys()) == ['u', 'v', 'w', 'x', 'y', 'z']
+    assert sorted(d.values()) == sorted([b'the', b'big', b'brown',
+                                         b'fox', b'jumps', b'over'])
+
+    assert a == {'u': b'the', 'v': b'big', 'x': b'fox'}
+    assert b == {'z': b'over'}
+    assert c == {'w': b'brown', 'y': b'jumps'}
+
+    # Changing existing keys can move values from one mapping to another.
+    d['w'] = b'lazy'
+    d['x'] = b'dog'
+    assert d['w'] == b'lazy'
+    assert d['x'] == b'dog'
+    assert len(d) == 6
+    assert sorted(d.values()) == sorted([b'the', b'big', b'lazy',
+                                         b'dog', b'jumps', b'over'])
+
+    assert a == {'u': b'the', 'v': b'big', 'x': b'dog'}
+    assert b == {'w': b'lazy', 'z': b'over'}
+    assert c == {'y': b'jumps'}
+
+    del d['v']
+    del d['w']
+    assert len(d) == 4
+    assert 'v' not in d
+    assert 'w' not in d
+    assert sorted(d.values()) == sorted([b'the', b'dog', b'jumps', b'over'])
+
+
+def test_mapping():
+    """
+    Test mapping interface for Sieve().
+    """
+    a = {}
+    b = {}
+    def selector(key, value):
+        return sum(bytearray(value)) & 1
+    mappings = {0: a, 1: b}
+    z = Sieve(mappings, selector)
+    utils_test.check_mapping(z)
--- zict/tests/utils_test.py
+++ zict/tests/utils_test.py
@@ -1,8 +1,72 @@
 from __future__ import absolute_import, division, print_function
 
 from collections import MutableMapping
+import random
+import string
 
 import pytest
+
+
+def generate_random_strings(n, min_len, max_len):
+    r = random.Random(42)
+    l = []
+    chars = string.ascii_lowercase + string.digits
+
+    for i in range(n):
+        nchars = r.randint(min_len, max_len)
+        s = ''.join(r.choice(chars) for _ in range(nchars))
+        l.append(s)
+
+    return l
+
+
+def to_bytestring(s):
+    if isinstance(s, bytes):
+        return s
+    else:
+        return s.encode('latin1')
+
+
+def check_items(z, expected_items):
+    items = list(z.items())
+    assert len(items) == len(expected_items)
+    assert sorted(items) == sorted(expected_items)
+    # All iterators should walk the mapping in the same order
+    assert list(z.keys()) == [k for k, v in items]
+    assert list(z.values()) == [v for k, v in items]
+    assert list(z) == [k for k, v in items]
+
+
+def stress_test_mapping_updates(z):
+    # Certain mappings shuffle between several underlying stores
+    # during updates.  This stress tests the internal mapping
+    # consistency.
+    r = random.Random(42)
+
+    keys = list(string.ascii_lowercase)
+    values = [to_bytestring(s)
+              for s in generate_random_strings(len(keys), 1, 10)]
+
+    z.clear()
+    assert len(z) == 0
+
+    for k, v in zip(keys, values):
+        z[k] = v
+    assert len(z) == len(keys)
+    assert sorted(z) == sorted(keys)
+    assert sorted(z.items()) == sorted(zip(keys, values))
+
+    for i in range(3):
+        r.shuffle(keys)
+        r.shuffle(values)
+        for k, v in zip(keys, values):
+            z[k] = v
+        check_items(z, list(zip(keys, values)))
+
+        r.shuffle(keys)
+        r.shuffle(values)
+        z.update(zip(keys, values))
+        check_items(z, list(zip(keys, values)))
 
 
 def check_mapping(z):
@@ -15,15 +79,11 @@ def check_mapping(z):
     assert len(z) == 0
 
     z['abc'] = b'456'
-    z['xyz'] = b'123'
+    z['xyz'] = b'12'
     assert len(z) == 2
     assert z['abc'] == b'456'
 
-    items = list(z.items())
-    assert set(items) == {('abc', b'456'), ('xyz', b'123')}
-    assert list(z.keys()) == [k for k, v in items]
-    assert list(z.values()) == [v for k, v in items]
-    assert list(z) == [k for k, v in items]
+    check_items(z, [('abc', b'456'), ('xyz', b'12')])
 
     assert 'abc' in z
     assert 'xyz' in z
@@ -33,11 +93,11 @@ def check_mapping(z):
         z['def']
 
     z.update(xyz=b'707', uvw=b'000')
-    assert set(z.items()) == {('abc', b'456'), ('xyz', b'707'), ('uvw', b'000')}
+    check_items(z, [('abc', b'456'), ('xyz', b'707'), ('uvw', b'000')])
     z.update([('xyz', b'654'), ('uvw', b'999')])
-    assert set(z.items()) == {('abc', b'456'), ('xyz', b'654'), ('uvw', b'999')}
+    check_items(z, [('abc', b'456'), ('xyz', b'654'), ('uvw', b'999')])
     z.update({'xyz': b'321'})
-    assert set(z.items()) == {('abc', b'456'), ('xyz', b'321'), ('uvw', b'999')}
+    check_items(z, [('abc', b'456'), ('xyz', b'321'), ('uvw', b'999')])
 
     del z['abc']
     with pytest.raises(KeyError):
@@ -52,3 +112,5 @@ def check_mapping(z):
     assert len(z) == 3
     assert z['def'] == b'\x00\xff'
     assert 'def' in z
+
+    stress_test_mapping_updates(z)
