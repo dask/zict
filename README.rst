3c2101aab150051945d752208217fb5aaf250292
Matthew Rocklin <mrocklin@gmail.com>
2017-06-28 09:25:58 -0400

    add links to readme and setup.py
    Fixes #17

--- README.rst
+++ README.rst
@@ -3,4 +3,8 @@ Zict
 
 |Build Status|
 
-Mutable Mapping interfaces
+Mutable Mapping interfaces.  See documentation_.
+
+.. _documentation: http://zict.readthedocs.io/en/latest/
+.. |Build Status| image:: https://travis-ci.org/dask/zict.svg?branch=master
+   :target: https://travis-ci.org/dask/zict
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 setup(name='zict',
       version='0.1.2',
       description='Mutable mapping tools',
-      url='http://github.com/dask/zict/',
+      url='http://zict.readthedocs.io/en/latest/',
       maintainer='Matthew Rocklin',
       maintainer_email='mrocklin@gmail.com',
       license='BSD',
