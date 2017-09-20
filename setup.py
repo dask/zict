0fa08ae6230bc563827b0254b7b634c3aaef3ff7
Matthew Rocklin <mrocklin@gmail.com>
2017-09-20 09:28:15 -0400

    bump version to 0.1.3

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ import os
 from setuptools import setup
 
 setup(name='zict',
-      version='0.1.2',
+      version='0.1.3',
       description='Mutable mapping tools',
       url='http://zict.readthedocs.io/en/latest/',
       maintainer='Matthew Rocklin',
       maintainer_email='mrocklin@gmail.com',
       license='BSD',
-      keywords='mutable mapping dict',
+      keywords='mutable mapping,dict,dask',
       packages=['zict'],
       install_requires=[open('requirements.txt').read().strip().split('\n')],
       long_description=(open('README.rst').read() if os.path.exists('README.rst')
