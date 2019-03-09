b26ae9f4f967d397a946fd9abbe74a38fcf4d2d0
Matthew Rocklin <mrocklin@gmail.com>
2019-03-09 08:35:28 -0800

    bump version to 0.1.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 from setuptools import setup
 
 setup(name='zict',
-      version='0.1.3',
+      version='0.1.4',
       description='Mutable mapping tools',
       url='http://zict.readthedocs.io/en/latest/',
       maintainer='Matthew Rocklin',
