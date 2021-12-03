f1d8c9ec25f3ba6e90c100e8254fe7b8292e540a
ncclementi <natyclementi@gmail.com>
2021-12-03 16:59:11 -0500

    tempary version bump for testing distributed

--- setup.py
+++ setup.py
@@ -1,24 +1,28 @@
 #!/usr/bin/env python
 
 import os
+
 from setuptools import setup
 
-setup(name='zict',
-      version='2.0.0',
-      description='Mutable mapping tools',
-      url='http://zict.readthedocs.io/en/latest/',
-      maintainer='Matthew Rocklin',
-      maintainer_email='mrocklin@gmail.com',
-      license='BSD',
-      keywords='mutable mapping,dict,dask',
-      packages=['zict'],
-      install_requires=open('requirements.txt').read().strip().split('\n'),
-      long_description=(open('README.rst').read() if os.path.exists('README.rst')
-                        else ''),
-      classifiers=[
-          "Programming Language :: Python",
-          "Programming Language :: Python :: 3.6",
-          "Programming Language :: Python :: 3.7",
-          "Programming Language :: Python :: 3.8",
-      ],
-      zip_safe=False)
+setup(
+    name="zict",
+    version="2.0.1",  # Fix this before mergin. Used to test distributed
+    description="Mutable mapping tools",
+    url="http://zict.readthedocs.io/en/latest/",
+    maintainer="Matthew Rocklin",
+    maintainer_email="mrocklin@gmail.com",
+    license="BSD",
+    keywords="mutable mapping,dict,dask",
+    packages=["zict"],
+    install_requires=open("requirements.txt").read().strip().split("\n"),
+    long_description=(
+        open("README.rst").read() if os.path.exists("README.rst") else ""
+    ),
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    zip_safe=False,
+)
--- zict/__init__.py
+++ zict/__init__.py
@@ -1,9 +1,10 @@
-from .zip import Zip
+from .buffer import Buffer
 from .file import File
 from .func import Func
-from .lru import LRU
-from .buffer import Buffer
-from .sieve import Sieve
 from .lmdb import LMDB
+from .lru import LRU
+from .sieve import Sieve
+from .zip import Zip
 
-__version__ = "2.0.0"
+# Fix this before mergin. Used to test distributed
+__version__ = "2.0.1"
