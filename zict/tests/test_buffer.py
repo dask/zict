f2d40e404df2279d2496a1bf69e41ba0f2d669ba
ncclementi <natyclementi@gmail.com>
2021-12-03 13:56:04 -0500

    add test for buffer (broken)

--- zict/tests/test_buffer.py
+++ zict/tests/test_buffer.py
@@ -1,11 +1,14 @@
-from zict import Buffer
+import pytest
+
+import zict
+
 from . import utils_test
 
 
 def test_simple():
     a = dict()
     b = dict()
-    buff = Buffer(a, b, n=10, weight=lambda k, v: v)
+    buff = zict.Buffer(a, b, n=10, weight=lambda k, v: v)
 
     buff["x"] = 1
     buff["y"] = 2
@@ -65,7 +68,7 @@ def test_setitem_avoid_fast_slow_duplicate():
 
     a = dict()
     b = dict()
-    buff = Buffer(a, b, n=10, weight=lambda k, v: v)
+    buff = zict.Buffer(a, b, n=10, weight=lambda k, v: v)
     for first, second in [(1, 12), (12, 1)]:
         buff["a"] = first
         assert buff["a"] == first
@@ -89,7 +92,7 @@ def test_mapping():
     """
     a = {}
     b = {}
-    buff = Buffer(a, b, n=2)
+    buff = zict.Buffer(a, b, n=2)
     utils_test.check_mapping(buff)
     utils_test.check_closing(buff)
 
@@ -107,7 +110,7 @@ def test_callbacks():
 
     a = dict()
     b = dict()
-    buff = Buffer(
+    buff = zict.Buffer(
         a,
         b,
         n=10,
@@ -136,3 +139,76 @@ def test_callbacks():
     buff["x"]
     assert f2s == ["x", "y"]
     assert s2f == ["x"]
+
+
+def test_callbacks_exception_catch():
+    class MyError(Exception):
+        pass
+
+    f2s = []
+
+    def f2s_cb(k, v):
+        if v > 10:
+            raise MyError()
+        f2s.append(k)
+
+    s2f = []
+
+    def s2f_cb(k, v):
+        s2f.append(k)
+
+    a = dict()
+    b = dict()
+    buff = zict.Buffer(
+        a,
+        b,
+        n=10,
+        weight=lambda k, v: v,
+        fast_to_slow_callbacks=f2s_cb,
+        slow_to_fast_callbacks=s2f_cb,
+    )
+
+    buff["x"] = 1
+    buff["y"] = 2
+
+    assert buff["x"] == 1
+    assert buff["y"] == 2
+    assert not f2s
+    assert not s2f
+    assert a == {"x": 1, "y": 2}  # keys are in fast/memory
+    assert not b
+
+    # Add key < n but total weight > n this will move x out of fast
+    buff["z"] = 8
+
+    assert f2s == ["x"]
+    assert s2f == []
+    assert a == {"y": 2, "z": 8}
+    assert b == {"x": 1}
+
+    # Add key > n, again total weight > n this will move everuthing to slow except w
+    # that stays in fast due after callback raise
+    with pytest.raises(MyError):
+        buff["w"] = 11
+
+    assert f2s == ["x", "y", "z"]
+    assert s2f == []
+    assert a == {"w": 11}
+    assert b == {"x": 1, "y": 2, "z": 8}
+
+    # Retrieve x from slow to fast, will try to move w to slow and raise
+    with pytest.raises(MyError):
+        buff["x"]
+
+    assert f2s == ["x", "y", "z"]
+    # Needs FIX
+    # the following is not working because in buffer.py slow_to_fast when adding
+    # the key to fast is trying to evict "w" to slow and raising (which is an obvious
+    # fail raise we might want to avoid), not getting to the callbacks.
+    # If I do try/except and pass in slow_to_fast things are handled well, but not sure the
+    # pass is the right call here.
+    assert s2f == [
+        "x"
+    ]  # this is currently empty because it's not getting to the callback
+    assert a == {"x": 1, "w": 11}
+    assert b == {"y": 2, "z": 8}
